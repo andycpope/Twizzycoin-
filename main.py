3ad878eb53f2f305c7bca1bfbfd1d80b14ad6516
andycpope <andycpope7@gmail.com>
2025-06-25 22:49:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import os
 import telebot
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "7625675407:AAHN2K4xZ3fNa4Ze_V47_BEjvuhDRyalJR4"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(func=lambda message: message.text.lower() in ["x", "ca", "website"])
 def handle_message(message):
     text = message.text.lower()
     if text == "x":
-        bot.reply_to(message, "Follow the $TWIZZY Twitter: https://twitter.com/andycpope")
+        bot.reply_to(message, "Follow the $TWIZZY Twitter: https://twitter.com/twizzycoin")
     elif text == "ca":
         bot.reply_to(message, "Contract Address: 8UN4HQbskKB4tLrwe6VEsqvr2HfnPDHg4fKdUYBipump")
     elif text == "website":
