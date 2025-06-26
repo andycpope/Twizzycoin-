88b27846dbdc014e5162607d40c1ba96e1c8ba25
andycpope <andycpope7@gmail.com>
2025-06-25 22:34:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome to $TWIZZY. Type 'ca' to get the contract address.")
+    bot.reply_to(message, "Welcome to $TWIZZY. Type 'ca' to get the contract address or 'x' to see our X page.")
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    if message.text.lower() == "ca":
+    text = message.text.lower()
+    
+    if text == "ca":
         bot.reply_to(message, "Contract Address: 8UN4HQbskKB4tLrwe6VEsqvr2HfnPDHg4fKdUYBipump")
+    elif text == "x":
+        bot.reply_to(message, "Follow us on X: https://twitter.com/TwizzyCoin")
     else:
-        bot.reply_to(message, "Thanks for your message.")
+        bot.reply_to(message, "Thanks for your message. Type 'ca' or 'x' for quick links.")
 
 bot.polling(none_stop=True)
